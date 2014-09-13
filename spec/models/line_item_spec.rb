6e88cc33b7eaec930f019f9ecdf84b30e0846f03
Justin Veirs <justinveirs@gmail.com>
2014-09-13 15:23:08 -0400

    holler

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe LineItem, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#total_price" do
+    let(:product) { double("Product", price: 50) }
+    let(:line_item) do
+      line_item = LineItem.new
+      line_item.stub(:product).and_return(product)
+      line_item
+    end
+
+    it "returns 100 for a quantity of 2" do
+      line_item.quantity = 2
+      expect(line_item.total_price).to eq(100)
+    end
+  end
 end
